4686928668e25c2c79f2b7e4e42d719b78ef092b
Sergio Chudnovets <134851371+Khair-ad-Din@users.noreply.github.com>
2025-04-26 17:37:56 +0200

    Commit Inicial

---
+++ src/main/java/com/mondo/erp/MondoErpApplication.java
@@ -0,0 +1,13 @@
+package com.mondo.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MondoErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MondoErpApplication.class, args);
+	}
+
+}
